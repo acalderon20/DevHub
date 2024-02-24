195c0490a62cda2ff4c8d47daecd42149d5cabbd
“acalderon20” <acalderon20@coe.edu>
2024-02-23 20:03:14 -0600

    Initial Commit

---
+++ DevHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DevHub
+//
+//  Created by Adolfo Calderon on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DevHub/DevHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevHubApp.swift
+//  DevHub
+//
+//  Created by Adolfo Calderon on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DevHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
