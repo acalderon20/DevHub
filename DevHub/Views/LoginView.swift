5104b0f407eafecfdfbef33b5cc3f5245e09728c
“acalderon20” <acalderon20@coe.edu>
2024-02-23 21:44:50 -0600

    Added MapView and LoginView

---
+++ DevHub/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  DevHub
+//
+//  Created by Adolfo Calderon on 2/23/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ DevHub/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  DevHub
+//
+//  Created by Adolfo Calderon on 2/23/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
